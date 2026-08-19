fdd8bd22edf82fe0e869e880d58086b7e3b2fa02
Aman pal <amanpal7983@gmail.com>
2026-08-19 15:14:18 +0530

    add new files

---
+++ arrraays/EnhancedForLoop.java
@@ -0,0 +1,11 @@
+package arrraays;
+
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        int[] arr = { 1,2,3,4,5,6,5,4,3,2,1};
+
+        for(int ele:arr ){
+            System.out.println(ele);
+        }
+    }
+}
