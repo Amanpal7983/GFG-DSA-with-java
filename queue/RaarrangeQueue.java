1f6a2d97ffd1bc2b7b966113b84db27accd7575c
Aman pal <amanpal7983@gmail.com>
2026-09-05 02:13:36 +0530

    adding new files

--- queue/RaarrangeQueue.java
+++ queue/RaarrangeQueue.java
@@ -19,3 +19,49 @@
 //     return q;
 //  }
 // }
+
+
+
+
+//Mehtod 2 via stack
+
+
+
+// static Queue<Integer> rearrange(Queue<Integer> q){
+//     Stack<Integer> st = new Stack<>();
+
+//     int n = q.size();
+    
+
+//     //half ele ko queue se stack me daalo
+//     for(int i = 1;i<=n/2;i++){
+//         st.push(q.remove());
+//     }
+
+//     //ab stack se queue me daalo
+//     while(st.size()>0){
+//         q.add(st.pop());
+//     }
+     
+//     //ab fir se queue se stack
+//     for(int i=1;i<=n/2;i++){
+//         st.push(q.remove());
+//     }
+
+//     //st se remove and q se add&remove
+
+//     while(st.size()>0){
+//         q.add(st.pop());
+//         q.add(q.remove());
+//     }
+
+//     //now reverse the stack
+//     while(q.size()>0){
+//         st.push(q.remove());
+//     }
+//     while(st.size()>0){
+//         q.add(st.pop());
+//     }
+
+//     return q;
+// }
